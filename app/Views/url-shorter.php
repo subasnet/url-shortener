db7c237e73b968f53916e038a04373ef16e7d0f7
Subash <subash.basnet@nokia.com>
2024-05-29 11:36:48 +0300

    Adding css and required plugins.

--- app/Views/url-shorter.php
+++ app/Views/url-shorter.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>URL Shortener</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
     </head>
     <body>
 
@@ -11,14 +14,18 @@
                 <input type="text" name="long_url" id="long_url" placeholder="Enter long URL" required>
                 <button id="shorten-btn">Shorten</button>
             </form>
-        </div>
-        <div id="short-url-container">
-            <p>Here's your shorten URL:</p>
-            <div id="short-url">
-                <a href="javascript:void(0)" target="_blank">Short URL</a>
-                <button type="button" id="copy-btn"></button>
+            <div id="short-url-container">
+                <p>Here's your shorten URL:</p>
+                <div id="short-url">
+                    <a href="javascript:void(0)" target="_blank">Short URL</a>
+                    <button type="button" id="copy-btn"></button>
+                </div>
             </div>
         </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     </body>
 </html>
